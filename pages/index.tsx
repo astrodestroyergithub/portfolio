571c1905333f3b58fe3cb6062efea634800eaa99
Tamojit Roy <astrodestroyer146@gmail.com>
2021-11-05 14:41:19 +0530

    Minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,10 +15,9 @@ const About: NextPage = () => {
   return (
     <div className="flex flex-col flex-grow px-6 pt-1 ">
       <h6 className="my-3 text-base font-medium">
-        I am currently pursuing B.Tech Degree(Final Year) in Computer Science
-        Engineering from Academy of Technology. I have 3+ years of experience in
-        Web Development and I have a Youtube Channel where I teach Full Stack
-        Web Development
+        I am currently pursuing B-Tech Degree (Pre-Final Year) in Computer Science
+        Engineering from Vellore Institute of Technology. I have 3+ years of experience in
+        Web Development and I have a Youtube Channel where I teach coding in various languages like C, C++, Java and Python. I also make videos on productivity and anything which fascinates me.
       </h6>
       <div
         className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
